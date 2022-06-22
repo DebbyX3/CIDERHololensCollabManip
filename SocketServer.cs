a2b47500b2295706a87d9104e20ecbd0417ad935
Deborah Pintani <p.debby100@yahoo.it>
2022-06-22 17:55:28 +0200

    added thread improvements to socket server

--- SocketServer.cs
+++ SocketServer.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -6,11 +6,15 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using TMPro;
 
 public class SocketServer : MonoBehaviour {
     System.Threading.Thread SocketThread;
     volatile bool keepReading = false;
     public GameObject cube;
+
+    public TMP_Text logText;
+    private string log = "";
 
 
     // Use this for initialization
@@ -56,11 +60,11 @@ public class SocketServer : MonoBehaviour {
         //IPEndPoint localEndPoint = new IPEndPoint(ipArray[0], 9025);
 
         //IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
-        //IPAddress ipAddress = ipHostInfo.AddressList[0];
-        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 60000);
+        IPAddress ipAddress = IPAddress.Any;
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 60000);
 
-        //Debug.Log(ipHostInfo.AddressList[0].ToString());
-        //Debug.Log(ipAddress);
+        Debug.Log(ipAddress);
+        log += ipAddress + "\n";
 
         // Create a TCP/IP socket.
         listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -78,9 +82,12 @@ public class SocketServer : MonoBehaviour {
 
                 // Program is suspended while waiting for an incoming connection.
                 Debug.Log("Waiting for Connection");     //It works
+                log += "Waiting for connection \n";
 
                 handler = listener.Accept();
                 Debug.Log("Client Connected");     //It doesn't work
+                log += "Client Connected \n";
+
                 data = null;
 
                 // An incoming connection needs to be processed.
@@ -88,6 +95,7 @@ public class SocketServer : MonoBehaviour {
                     bytes = new byte[1024];
                     int bytesRec = handler.Receive(bytes);
                     Debug.Log("Received from Server");
+                    log += "Received from Server \n";
 
                     if (bytesRec <= 0) {
                         keepReading = false;
@@ -110,6 +118,15 @@ public class SocketServer : MonoBehaviour {
         }
     }
 
+    private void Update() 
+    {
+        if (!log.Equals("")) 
+        {
+            logText.text += log;
+            log = "";
+        }
+    }
+
     void stopServer() {
         keepReading = false;
 
@@ -121,10 +138,193 @@ public class SocketServer : MonoBehaviour {
         if (handler != null && handler.Connected) {
             handler.Disconnect(false);
             Debug.Log("Disconnected!");
+            log += "Disconnected \n";
         }
     }
 
     void OnDisable() {
         stopServer();
+    }
+}
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using TMPro;
+
+public class SocketServer : MonoBehaviour {
+    private Thread waitingForConnectionsThread;
+    private Thread handleConnectionThread;
+
+    volatile bool keepReading = false;
+    public GameObject cube;
+
+    // can be changed in unity inspector
+    public int portToListen = 60000;
+
+    private Socket listener;
+    private Socket handler;
+
+    private IPEndPoint localEndPoint;
+
+    public TMP_Text logText;
+    private string log = "";
+
+    protected void StartServer() 
+    {
+        CreateServerListener();
+        BindToLocalEndPoint();
+
+        waitingForConnectionsThread = new Thread(WaitingForConnections);
+        waitingForConnectionsThread.IsBackground = true;
+        waitingForConnectionsThread.Start();
+    }
+
+    private void Update() {
+        if (!log.Equals("")) {
+            logText.text += log;
+            log = "";
+        }
+
+        Debug.Log("Groda");
+    }
+
+
+    private void WaitingForConnections() {
+        try {
+            while (true) {
+                ListenToClient();
+
+                keepReading = true;
+
+                Debug.Log("Waiting for Connection");
+                log += "Waiting for Connection \n";
+
+                // Program is suspended while waiting for an incoming connection.
+                handler = listener.Accept();
+
+                Debug.Log("Client Connected");
+                log += "Client Connected \n";
+
+                //create thread to handle request
+                //SocketThread = new Thread(NetworkCode(handler));
+                handleConnectionThread = new Thread(() => RequestHandler(handler));
+                handleConnectionThread.IsBackground = true;
+                handleConnectionThread.Start();
+            }
+        } catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    void RequestHandler(Socket handler) {
+        string data = null;
+        byte[] bytes = new Byte[1024]; // Data buffer for incoming data.
+        int bytesRec = 0;
+
+        try {
+            // An incoming connection needs to be processed.
+            while (keepReading) {
+                bytes = new byte[1024];
+                bytesRec = handler.Receive(bytes);
+
+                Debug.Log("Received from Client");
+                log += "Received from client \n";
+
+                Debug.Log("Received bytes from client: " + bytesRec);
+                Debug.Log("Message from client: " + Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+
+                if (bytesRec <= 0) {
+                    keepReading = false;
+                    handler.Disconnect(true);
+                    Debug.Log("Disconnected while waiting for more messages");
+                    break;
+                }
+
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                if (data.IndexOf("<EOF>") > -1) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    protected void CreateServerListener() {
+        // host running the application.
+        IPAddress ipAddress = IPAddress.Any;
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 60000);
+
+        Debug.Log(ipAddress);
+        log += ipAddress + "\n";
+
+        // Create a TCP/IP socket.
+        listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        this.localEndPoint = localEndPoint;
+    }
+
+    private void BindToLocalEndPoint() {
+        try {
+            listener.Bind(localEndPoint);
+        } catch (SocketException se) {
+            Debug.Log("An error occurred when attempting to access the socket.\n\n" + se.ToString());
+        } catch (ObjectDisposedException ode) {
+            Debug.Log("The Socket has been closed.\n\n" + ode.ToString());
+        } catch (ArgumentNullException ane) {
+            Debug.Log("The local endpoint is null.\n\n" + ane.ToString());
+        }
+    }
+
+    private void ListenToClient() {
+        try {
+            listener.Listen(10);
+        } catch (SocketException se) {
+            Debug.Log("An error occurred when attempting to access the socket.\n\n" + se.ToString());
+        } catch (ObjectDisposedException ode) {
+            Debug.Log("The Socket has been closed.\n\n" + ode.ToString());
+        } catch (ArgumentNullException ane) {
+            Debug.Log("The local endpoint is null.\n\n" + ane.ToString());
+        }
+    }
+
+
+    protected void StopServer() {
+        keepReading = false;
+
+        if (handler != null && handler.Connected) {
+            // there's no need of shutdown and disconnect a listener socket!
+            //listener.Shutdown(SocketShutdown.Both);
+            //listener.Disconnect(false);
+            listener.Close();
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Disconnect(false);
+            handler.Close();
+
+            Debug.Log("Disconnected!");
+            log += "Disconnected! \n";
+        }
+
+        //stop thread
+        if (waitingForConnectionsThread != null) {
+            Debug.Log("abort");
+            log += "abort \n";
+
+            waitingForConnectionsThread.Abort();
+            handleConnectionThread.Abort();
+        }
+    }
+
+    void OnDisable() {
+        StopServer();
     }
 }
--- SocketServerMain.cs
+++ SocketServerMain.cs
@@ -7,17 +7,12 @@ public class SocketServerMain : SocketServer
 {
     public bool connessioneRiuscita = false;
 
-    // Start is called before the first frame update
     void Start() {
-        //apro connessione con py
-        Debug.Log("Eseguo conn " + gameObject.name);
-        Application.runInBackground = true;
+        //open connection
+        Debug.Log("Eseguo conn con " + gameObject.name);
         StartServer();
-
-        cube.GetComponent<Renderer>().material.color = new Color(0, 204, 200);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
