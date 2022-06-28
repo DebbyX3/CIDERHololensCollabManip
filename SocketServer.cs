de72a4cd6cb0a86233bf7ce4610e99c2a173c895
Deborah Pintani <p.debby100@yahoo.it>
2022-06-28 16:23:16 +0200

    Added ConcurrentQueue

--- SocketServer.cs
+++ SocketServer.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+//using System.Runtime.InteropServices;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -8,12 +9,15 @@ using System.Threading;
 using UnityEngine;
 using TMPro;
 
-public class SocketServer : MonoBehaviour {
+using MeshMessage = System.Collections.Generic.KeyValuePair<int, UnityEngine.Mesh>;
+public class SocketServer : MonoBehaviour
+{
     private Thread waitingForConnectionsThread;
     private Thread handleConnectionThread;
 
     volatile bool keepReading = false;
     public GameObject cube;
+    public GameObject sphere;
 
     // can be changed in unity inspector
     public int portToListen = 60000;
@@ -25,8 +29,10 @@ public class SocketServer : MonoBehaviour {
 
     public TMP_Text logText;
     private string log = "";
+    
+    public ConcurrentQueue<MeshMessage> meshMessages = new ConcurrentQueue<MeshMessage>();
 
-    protected void StartServer() 
+    protected void StartServer()
     {
         CreateServerListener();
         BindToLocalEndPoint();
@@ -36,8 +42,10 @@ public class SocketServer : MonoBehaviour {
         waitingForConnectionsThread.Start();
     }
 
-    private void Update() {
-        if (!log.Equals("")) {
+    private void Update()
+    {
+        if (!log.Equals(""))
+        {
             logText.text += log;
             log = "";
         }
@@ -46,9 +54,12 @@ public class SocketServer : MonoBehaviour {
     }
 
 
-    private void WaitingForConnections() {
-        try {
-            while (true) {
+    private void WaitingForConnections()
+    {
+        try
+        {
+            while (true)
+            {
                 ListenToClient();
 
                 keepReading = true;
@@ -68,26 +79,32 @@ public class SocketServer : MonoBehaviour {
                 handleConnectionThread.IsBackground = true;
                 handleConnectionThread.Start();
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             Debug.Log(e.ToString());
         }
     }
 
-    void RequestHandler(Socket handler) {
+    void RequestHandler(Socket handler)
+    {
         string data = null;
         byte[] bytes = new Byte[1024]; // Data buffer for incoming data.
         int bytesRec = 0;
 
-        try {
+        try
+        {
             // An incoming connection needs to be processed.
-            while (keepReading) {
+            while (keepReading)
+            {
                 bytes = new byte[1024];
                 bytesRec = handler.Receive(bytes);
 
                 Debug.Log("Received from Client");
                 log += "Received from client \n";
 
-                if (bytesRec <= 0) {
+                if (bytesRec <= 0)
+                {
                     keepReading = false;
                     handler.Disconnect(true);
                     Debug.Log("Disconnected while waiting for more messages");
@@ -96,12 +113,19 @@ public class SocketServer : MonoBehaviour {
 
                 data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
 
-                if (data.IndexOf("<EOF>") > -1) {
+                if (data.IndexOf("<EOF>") > -1)
+                {
                     break;
                 }
 
                 Debug.Log("Received bytes from client: " + bytesRec);
                 Debug.Log("Message from client: " + data);
+
+                List<Mesh> deserializedMeshed = new List<Mesh>();
+                deserializedMeshed = (List<Mesh>)SimpleMeshSerializer.Deserialize(bytes);
+
+                meshMessages.Enqueue(new MeshMessage(0, deserializedMeshed[0]));
+
 
                 // Echo the data back to the client.  
                 byte[] msg = Encoding.ASCII.GetBytes(data);
@@ -110,15 +134,17 @@ public class SocketServer : MonoBehaviour {
                 handler.Shutdown(SocketShutdown.Both);
                 handler.Close();
                 keepReading = false;
+
             }
-        } 
-        catch (Exception e) 
+        }
+        catch (Exception e)
         {
             Debug.Log(e.ToString());
         }
     }
 
-    protected void CreateServerListener() {
+    protected void CreateServerListener()
+    {
         // host running the application.
         IPAddress ipAddress = IPAddress.Any;
         IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 60000);
@@ -132,35 +158,53 @@ public class SocketServer : MonoBehaviour {
         this.localEndPoint = localEndPoint;
     }
 
-    private void BindToLocalEndPoint() {
-        try {
+    private void BindToLocalEndPoint()
+    {
+        try
+        {
             listener.Bind(localEndPoint);
-        } catch (SocketException se) {
+        }
+        catch (SocketException se)
+        {
             Debug.Log("An error occurred when attempting to access the socket.\n\n" + se.ToString());
-        } catch (ObjectDisposedException ode) {
+        }
+        catch (ObjectDisposedException ode)
+        {
             Debug.Log("The Socket has been closed.\n\n" + ode.ToString());
-        } catch (ArgumentNullException ane) {
+        }
+        catch (ArgumentNullException ane)
+        {
             Debug.Log("The local endpoint is null.\n\n" + ane.ToString());
         }
     }
 
-    private void ListenToClient() {
-        try {
+    private void ListenToClient()
+    {
+        try
+        {
             listener.Listen(10);
-        } catch (SocketException se) {
+        }
+        catch (SocketException se)
+        {
             Debug.Log("An error occurred when attempting to access the socket.\n\n" + se.ToString());
-        } catch (ObjectDisposedException ode) {
+        }
+        catch (ObjectDisposedException ode)
+        {
             Debug.Log("The Socket has been closed.\n\n" + ode.ToString());
-        } catch (ArgumentNullException ane) {
+        }
+        catch (ArgumentNullException ane)
+        {
             Debug.Log("The local endpoint is null.\n\n" + ane.ToString());
         }
     }
 
 
-    protected void StopServer() {
+    protected void StopServer()
+    {
         keepReading = false;
 
-        if (handler != null && handler.Connected) {
+        if (handler != null && handler.Connected)
+        {
             // there's no need of shutdown and disconnect a listener socket!
             //listener.Shutdown(SocketShutdown.Both);
             //listener.Disconnect(false);
@@ -175,7 +219,8 @@ public class SocketServer : MonoBehaviour {
         }
 
         //stop thread
-        if (waitingForConnectionsThread != null) {
+        if (waitingForConnectionsThread != null)
+        {
             Debug.Log("abort");
             log += "abort \n";
 
@@ -184,7 +229,8 @@ public class SocketServer : MonoBehaviour {
         }
     }
 
-    void OnDisable() {
+    void OnDisable()
+    {
         StopServer();
     }
 }
--- SocketServerMain.cs
+++ SocketServerMain.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
 
+using MeshMessage = System.Collections.Generic.KeyValuePair<int, UnityEngine.Mesh>;
 public class SocketServerMain : SocketServer
 {
     public bool connessioneRiuscita = false;
 
-    void Start() {
+    void Start()
+    {
         //open connection
         Debug.Log("Eseguo conn con " + gameObject.name);
         StartServer();
@@ -15,6 +17,14 @@ public class SocketServerMain : SocketServer
 
     void Update()
     {
-        
+        MeshMessage item;
+
+        if (!meshMessages.IsEmpty)
+        {
+            meshMessages.TryDequeue(out item);
+            SimpleMeshSerializer.CreateMesh(item);
+
+            sphere.GetComponent<MeshFilter>().mesh = item.Value;
+        }
     }
 }
