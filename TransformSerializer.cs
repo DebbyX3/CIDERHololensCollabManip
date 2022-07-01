514a3ea0c614ab5a8f30d9fbaa5c324b54aa1a3d
Deborah Pintani <p.debby100@yahoo.it>
2022-07-01 17:31:26 +0200

    Fixed returned values in TransformSerializer

--- TransformSerializer.cs
+++ TransformSerializer.cs
@@ -6,57 +6,41 @@ using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using SysDiag = System.Diagnostics;
 
-public class TransformSerializer : MonoBehaviour
+//https://stackoverflow.com/questions/39345820/easy-way-to-write-and-read-some-transform-to-a-text-file-in-unity3d
+[Serializable]
+public struct SerializableTransform
 {
-    /*
-    [Serializable]
-    public struct SerializebleVector {
-        public float x, y, z, w;
+    public Vector3 Position { get; private set; }
+    public Quaternion Rotation { get; private set; }
+    public Vector3 Scale { get; private set; }
 
-        public SerializebleVector(float x, float y, float z, float w = 0f) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.w = w;
-        }
-
-        public static explicit operator SerializebleVector(Quaternion a) {
-            return new SerializebleVector(a.x, a.y, a.z, a.w);
-        }
-
-        public static implicit operator SerializebleVector(Vector3 a) {
-            return new SerializebleVector(a.x, a.y, a.z);
-        }
-
-    }*/
-
-    //https://stackoverflow.com/questions/39345820/easy-way-to-write-and-read-some-transform-to-a-text-file-in-unity3d
-    [Serializable]
-    public struct SerializableTransform {
-        Vector3 position;
-        Quaternion rotation;
-        Vector3 scale;
-
-        public SerializableTransform(Transform tr) {
-            position = tr.position;
-            rotation = tr.rotation;
-            scale = tr.lossyScale;
-        }
-
-        public SerializableTransform(Vector3 position, Quaternion rotation, Vector3 scale) {
-            this.position = position;
-            this.rotation = rotation;
-            this.scale = scale;
-        }
+    public SerializableTransform(Transform tr)
+    {
+        Position = tr.position;
+        Rotation = tr.rotation;
+        Scale = tr.lossyScale;
     }
 
-    public static byte[] Serialize(IEnumerable<Transform> transforms) 
+    public SerializableTransform(Vector3 position, Quaternion rotation, Vector3 scale)
     {
+        this.Position = position;
+        this.Rotation = rotation;
+        this.Scale = scale;
+    }
+}
+
+public static class TransformSerializer
+{
+        public static byte[] Serialize(IEnumerable<Transform> transforms)
+    {        
         byte[] data = null;
 
-        using (MemoryStream stream = new MemoryStream()) {
-            using (BinaryWriter writer = new BinaryWriter(stream)) {
-                foreach (Transform transform in transforms) {
+        using (MemoryStream stream = new MemoryStream())
+        {
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                foreach (Transform transform in transforms)
+                {
                     WriteTransform(writer, transform);
                 }
 
@@ -69,11 +53,13 @@ public class TransformSerializer : MonoBehaviour
         return data;
     }
 
-    public static byte[] Serialize(Transform transform) {
+    public static byte[] Serialize(Transform transform)
+    {
         return Serialize(new List<Transform>() { transform });
     }
 
-    private static void WriteTransform(BinaryWriter writer, Transform transform) {
+    private static void WriteTransform(BinaryWriter writer, Transform transform)
+    {
         SysDiag.Debug.Assert(writer != null);
 
         // Write the transform data.
@@ -82,7 +68,8 @@ public class TransformSerializer : MonoBehaviour
         WriteScale(writer, transform.lossyScale);
     }
 
-    private static void WritePosition(BinaryWriter writer, Vector3 position) {
+    private static void WritePosition(BinaryWriter writer, Vector3 position)
+    {
         SysDiag.Debug.Assert(writer != null);
 
         writer.Write(position.x);
@@ -90,7 +77,8 @@ public class TransformSerializer : MonoBehaviour
         writer.Write(position.z);
     }
 
-    private static void WriteRotation(BinaryWriter writer, Quaternion rotation) {
+    private static void WriteRotation(BinaryWriter writer, Quaternion rotation)
+    {
         SysDiag.Debug.Assert(writer != null);
 
         writer.Write(rotation.x);
@@ -99,7 +87,8 @@ public class TransformSerializer : MonoBehaviour
         writer.Write(rotation.w);
     }
 
-    private static void WriteScale(BinaryWriter writer, Vector3 scale) {
+    private static void WriteScale(BinaryWriter writer, Vector3 scale)
+    {
         SysDiag.Debug.Assert(writer != null);
 
         writer.Write(scale.x);
@@ -108,12 +97,14 @@ public class TransformSerializer : MonoBehaviour
     }
 
     // can't create a transform object deatached from a gameobject, so i'll return a struct
-    public static IEnumerable<SerializableTransform> DeserializeMultiple(byte[] data) 
+    public static IEnumerable<SerializableTransform> DeserializeMultiple(byte[] data)
     {
         var transforms = new List<SerializableTransform>();
 
-        using (MemoryStream stream = new MemoryStream(data)) {
-            using (BinaryReader reader = new BinaryReader(stream)) {
+        using (MemoryStream stream = new MemoryStream(data))
+        {
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
                 //while (reader.BaseStream.Length - reader.BaseStream.Position >= HeaderSize) {
                 transforms.Add(ReadTransform(reader));
                 //}
@@ -123,14 +114,14 @@ public class TransformSerializer : MonoBehaviour
         return transforms;
     }
 
-    public static SerializableTransform Deserialize(byte[] data) {
+    public static SerializableTransform Deserialize(byte[] data)
+    {
         return ((List<SerializableTransform>)DeserializeMultiple(data))[0];
     }
 
-    private static SerializableTransform ReadTransform(BinaryReader reader) {
+    private static SerializableTransform ReadTransform(BinaryReader reader)
+    {
         SysDiag.Debug.Assert(reader != null);
-
-        SerializableTransform transform;
 
         // Read the transform data.
         Vector3 position = ReadPosition(reader);
@@ -140,34 +131,47 @@ public class TransformSerializer : MonoBehaviour
         return new SerializableTransform(position, rotation, scale);
     }
 
-    private static Vector3 ReadPosition(BinaryReader reader) {
+    private static Vector3 ReadPosition(BinaryReader reader)
+    {
         SysDiag.Debug.Assert(reader != null);
 
-        int x = reader.ReadInt32();
-        int y = reader.ReadInt32();
-        int z = reader.ReadInt32();
+        float x = reader.ReadSingle(); //read float
+        float y = reader.ReadSingle();
+        float z = reader.ReadSingle();
 
-        return new Vector3(x, y, x);
+        return new Vector3(x, y, z);
     }
 
-    private static Quaternion ReadRotation(BinaryReader reader) {
+    private static Quaternion ReadRotation(BinaryReader reader)
+    {
         SysDiag.Debug.Assert(reader != null);
 
-        int x = reader.ReadInt32();
-        int y = reader.ReadInt32();
-        int z = reader.ReadInt32();
-        int w = reader.ReadInt32();
+        float x = reader.ReadSingle();
+        float y = reader.ReadSingle();
+        float z = reader.ReadSingle();
+        float w = reader.ReadSingle();
 
-        return new Quaternion(x, y, x, w);
+        return new Quaternion(x, y, z, w);
     }
 
-    private static Vector3 ReadScale(BinaryReader reader) {
+    private static Vector3 ReadScale(BinaryReader reader)
+    {
         SysDiag.Debug.Assert(reader != null);
 
-        int x = reader.ReadInt32();
-        int y = reader.ReadInt32();
-        int z = reader.ReadInt32();
+        float x = reader.ReadSingle();
+        float y = reader.ReadSingle();
+        float z = reader.ReadSingle();
 
-        return new Vector3(x, y, x);
+        return new Vector3(x, y, z);
     }
+
+    //da mettere in un'altra classe probabilmente
+    //https://answers.unity.com/questions/1296012/best-way-to-set-game-object-transforms.html
+    public static void LoadTransform(this Transform originalTransform, SerializableTransform deserializedTransform)
+    {
+        originalTransform.position = deserializedTransform.Position;
+        originalTransform.rotation = deserializedTransform.Rotation;
+        originalTransform.localScale = deserializedTransform.Scale;
+    }
+    
 }
