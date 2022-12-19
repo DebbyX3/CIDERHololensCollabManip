656aea85a985fe2b99e5b99d32b0400d8c9183cd
DebbyX3 <p.debby100@yahoo.it>
2022-12-19 17:12:11 +0100

    Add new furniture

--- Assets/Scripts/ObjectsFiles.cs
+++ Assets/Scripts/ObjectsFiles.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using Newtonsoft.Json;
 using System;
 using System.Text;
 using System.Runtime.Serialization.Formatters.Binary;
